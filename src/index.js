4553c99745f09f8cdc803dd66a92c3d6227286a5
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-07-19 11:33:55 +0700

    add pwa

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import MainPage from "./components/main_page/MainPage";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 // import style
 import "./styles/style.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<MainPage />);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
